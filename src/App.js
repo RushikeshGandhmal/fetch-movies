05025b16fa86c1961e48868fcd44e63e91dc29ac
Rushi <rushikeshgandhmal@gmail.com>
2023-04-21 20:32:35 +0530

    first commit, sending http request using fetch

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+
+import MoviesList from "./components/MoviesList";
+import "./App.css";
+
+const App = () => {
+  const [movies, setMovies] = useState([]);
+
+  const fetchMoviesHandler = () => {
+    fetch("https://swapi.py4e.com/api/films/")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const transformedMovies = data.results.map((movieData) => {
+          return {
+            id: movieData.episode_id,
+            title: movieData.title,
+            openingText: movieData.opening_crawl,
+            releaseDate: movieData.release_date,
+          };
+        });
+        setMovies(transformedMovies);
+      });
+  };
+
+  return (
+    <React.Fragment>
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        <MoviesList movies={movies} />
+      </section>
+    </React.Fragment>
+  );
+};
+
+export default App;
