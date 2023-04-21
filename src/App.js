7d72deb3e8f1a2f396b64d22dcae1d3a7b18b287
Rushi <rushikeshgandhmal@gmail.com>
2023-04-21 22:12:39 +0530

    added loading handler logic and spinning component

--- src/App.js
+++ src/App.js
@@ -2,26 +2,26 @@ import React, { useState } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
+import Loading from "./components/UI/Loading";
 
 const App = () => {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-  const fetchMoviesHandler = () => {
-    fetch("https://swapi.py4e.com/api/films/")
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        const transformedMovies = data.results.map((movieData) => {
-          return {
-            id: movieData.episode_id,
-            title: movieData.title,
-            openingText: movieData.opening_crawl,
-            releaseDate: movieData.release_date,
-          };
-        });
-        setMovies(transformedMovies);
-      });
+  const fetchMoviesHandler = async () => {
+    setIsLoading(true);
+    const response = await fetch("https://swapi.py4e.com/api/films/");
+    const data = await response.json();
+    const transformedMovies = data.results.map((movieData) => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date,
+      };
+    });
+    setMovies(transformedMovies);
+    setIsLoading(false);
   };
 
   return (
@@ -30,7 +30,9 @@ const App = () => {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && <p>Found no movies.</p>}
+        {isLoading && <Loading />}
       </section>
     </React.Fragment>
   );
---
+++ src/components/UI/Loading.js
@@ -0,0 +1,7 @@
+import styles from "./Loading.module.css";
+
+const Loading = (props) => {
+  return <div className={styles.loader}></div>;
+};
+
+export default Loading;
